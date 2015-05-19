733cdb007d3314fb9b8b36b6fafb078e73a40c63
thiesen <nathan.thiesen@outlook.com>
2015-05-19 16:27:39 -0300

    split in several files

---
+++ board.rb
@@ -0,0 +1,51 @@
+module Spregen
+  class Board
+
+    attr_accessor :name
+
+    def initialize(board_name)
+      self.name = board_name
+    end
+
+    def lists
+      @lists ||= board.lists
+    end
+
+    def board
+      @board ||= Trello::Board.all.find { |x| x.name == self.name }
+    end
+
+    def bug_cards
+      @bug_cards ||= done_cards.find_all { |card| card.name =~ /\[BUG\]/i } if done_cards
+    end
+
+    def done_cards
+      @done_cards ||= get_cards(/^\[DONE\]/i)
+    end
+
+    def qa_cards
+      @qa_cards ||= get_cards(/^\[Q\.A\]/i)
+    end
+
+    def doing_cards
+      @doing_cards ||= get_cards(/^\[DOING\]/i)
+    end
+
+    def todo_cards
+      @todo_cards ||= get_cards(/^\[TODO\]/i)
+    end
+
+    private
+
+    def get_cards(list_name)
+      card_list = lists.detect { |list| list.name =~ list_name }
+      return unless card_list.present?
+      card_list.cards.reject do |card|
+        card.labels.find { |label| label.name == 'Skip Report' }
+      end
+    end
+
+
+  end
+
+end
---
+++ report.rb
@@ -0,0 +1,54 @@
+require_relative 'board'
+require_relative 'burndown'
+require_relative 'helpers'
+
+module Spregen
+  class Report
+
+    include Spregen::Helpers
+
+    attr_accessor :report
+
+    def initialize(configuration = {})
+      Spregen::Config.configure_trello_client
+      @board = Spregen::Board.new(configuration[:board_name])
+    end
+
+    def generate
+      file << print_report
+      file.close
+    end
+
+    def generate_burndown
+      burndown = Spregen::Burndown.new({})
+      burndown.generate
+    end
+
+    def print_report
+      puts Spregen::Template.build(binding)
+    end
+
+    def source
+      self
+    end
+
+    def title
+      "Report #{@board.name}"
+    end
+
+    def not_done
+      buffer = ""
+      rprint @board.doing_cards, buffer
+      rprint @board.todo_cards, buffer
+    end
+
+    def done
+      rprint @board.done_cards
+    end
+
+    def bugs
+      rprint @board.bug_cards
+    end
+
+  end
+end
