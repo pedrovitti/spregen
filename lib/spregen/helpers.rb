fe8d48235183e3e9059c05652c3434c11287a140
luciano <marcelinolucianom@gmail.com>
2015-09-15 08:41:24 -0300

    remove estimate points from cardn name

--- lib/spregen/helpers.rb
+++ lib/spregen/helpers.rb
@@ -1,6 +1,8 @@
 module Spregen
   module Helpers
 
+    ESTIMATE_POINTS_PATTERN = /\(\d(\.\d)?\)(.+)\[\d(\.\d)?\]$/
+
     def file
       @file ||= File.open("#{@source.name}-report-#{today}.md", "w")
     end
@@ -9,8 +11,9 @@ module Spregen
       return unless cards
       cards.each do |card|
         link = card.desc.split("\n").first || card.url
+        card_name = remove_estimate_points(card.name)
         buffer << "\n" unless buffer.blank?
-        buffer << " - [#{card.name}](#{link})"
+        buffer << " - [#{card_name}](#{link})"
       end
       buffer
     end
@@ -19,6 +22,9 @@ module Spregen
       Time.now.strftime("%d-%m-%Y-%M%S")
     end
 
+    private
+    def remove_estimate_points(card_name)
+      card_name.match(ESTIMATE_POINTS_PATTERN).match(2)
+    end
   end
-
 end
